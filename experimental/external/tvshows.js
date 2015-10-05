da4adaabfa8b565eba746298361e32f3babf6549
Max Soe <maxsoe@users.noreply.github.com>
2015-10-05 18:34:35 +0100

    Remove Bob's Burgers

--- experimental/external/tvshows.js
+++ experimental/external/tvshows.js
@@ -64,7 +64,6 @@
 			show_title == "Arrow" ||
 			show_title == "Avengers Assemble" ||
 			show_title == "Beware the Batman" ||
-			show_title == "Bob\'s Burgers" ||
 			show_title == "The Big Bang Theory" ||
 			show_title == "Brooklyn Nine-Nine" ||
 			show_title == "Californication" ||
