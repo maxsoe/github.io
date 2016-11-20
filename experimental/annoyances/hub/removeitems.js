bbcd74dc0e3ad879bbc023a6928580d5407df0c0
Max Soe <maxsoe@gmail.com>
2016-11-20 11:40:53 +0000

    testing

--- experimental/annoyances/hub/removeitems.js
+++ experimental/annoyances/hub/removeitems.js
@@ -1,9 +1,6 @@
 
 console.log("This is the correct branch");
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>;
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>;
 
 // var container = document.getElementsByClassName("sectionWrapper");
 
