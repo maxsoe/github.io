cca78706af48a1ecffbf9c220c53a22daa9cbf04
Max Soe <maxsoe@gmail.com>
2017-09-11 22:27:14 +0100

    pagination working in a buggy manner

--- experimental/chromeextensions/normal/mapmyride/myscript.js
+++ experimental/chromeextensions/normal/mapmyride/myscript.js
@@ -1,49 +1,28 @@
 $(window).ready(function() {
   console.log("Max's mapmyride script is ready to go");
 
-  // Insert CSS
-  // var path = chrome.extension.getURL('trakttv/mycss.css');
-  // $('head').append($('<link>')
-  //     .attr("rel","stylesheet")
-  //     .attr("type","text/css")
-  //     .attr("href", path));
-
   // Insert HTML file
   insertThisFile('mapmyride/myhtml.html');
 });
 
 // Update the contents of the page when using pagination
-// $(".button-j8tje").click (function() {
-//   insertThisFile('mapmyride/myhtml.html');
-// });
-
-// Watch a show
-$(".watch-now").click (function () {
-  // console.log("Watch now content: ",$("#watch-now-content").get());
-
-  insertThisFile('trakttv/myhtml.html');
-
-  // Get html doc
-  // var myURL = chrome.extension.getURL('trakttv/myhtml.html');
-  //
-  // $.get( myURL, function( myHTML ) {
-  //   console.log("myhtml is : ", myHTML);
-  //   var newItem = $(myHTML).filter('.test')[0].outerHTML;
-  //   $("#watch-now-content").prepend(newItem);
-  // });
+$(".container-33o3x").on("click", ".default-1VxRe", function() {
+   $(this).css({"backgroundColor" : "green"});
+  //  console.log("button clicked");
+   insertThisFile('mapmyride/myhtml.html');
 });
 
 function insertThisFile(myURL) {
   var processedURL = chrome.extension.getURL(myURL);
-  console.log("myURL is", myURL);
-  console.log("myURL is recognised as", processedURL);
+  // console.log("myURL is", myURL);
+  // console.log("myURL is recognised as", processedURL);
   $.get( processedURL, function( myHTML ) {
     // console.log("myHTML is: ", myHTML);
     var currentPageContent = $(".tableContainer-3g8hA tbody").get();
-    console.log("Content on the current page is: ", currentPageContent);
+    // console.log("Content on the current page is: ", currentPageContent);
     var rowArray = currentPageContent[0].children;
-    console.log("the children are: ", rowArray);
-    console.log("row length is: ", rowArray.length);
+    // console.log("the children are: ", rowArray);
+    // console.log("row length is: ", rowArray.length);
 
     // Set up container in HTML
     var myContainer = '<div class="strava_clone container"></div>'
@@ -54,41 +33,39 @@ function insertThisFile(myURL) {
       // console.log("row[" +i +"] is ", columnArray);
 
       var map = columnArray[0].firstChild.firstChild;
-      console.log("map " +i +": ", map);
+      // console.log("map " +i +": ", map);
 
       var date = columnArray[1].firstChild.firstChild;
-      console.log("date " +i +": ", date);
+      // console.log("date " +i +": ", date);
 
       var distance = columnArray[2].firstChild.firstChild;
-      console.log("distance " +i +": ", distance);
+      // console.log("distance " +i +": ", distance);
 
       var distanceUnit = columnArray[2].firstChild.children[1];
-      console.log("distance unit " +i +": ", distanceUnit);
+      // console.log("distance unit " +i +": ", distanceUnit);
 
       var elevation = columnArray[3].firstChild.firstChild;
-      console.log("elevation " +i +": ", elevation);
+      // console.log("elevation " +i +": ", elevation);
 
       var elevationUnit = columnArray[3].firstChild.children[1];
-      console.log("elevation unit " +i +": ", elevationUnit);
+      // console.log("elevation unit " +i +": ", elevationUnit);
 
       var name = columnArray[4].firstChild;
-      console.log("name " +i +": ", name);
+      // console.log("name " +i +": ", name);
 
       var city = columnArray[5].firstChild;
-      console.log("city " +i +": ", city);
+      // console.log("city " +i +": ", city);
 
       var privacy = columnArray[6].firstChild;
-      console.log("privacy " +i +": ", privacy);
+      // console.log("privacy " +i +": ", privacy);
 
       var copy = columnArray[7].children[0];
-      console.log("copy " +i +": ", copy);
+      // console.log("copy " +i +": ", copy);
 
       var edit = columnArray[7].children[1];
-      console.log("ride " +i +": ", edit);
+      // console.log("ride " +i +": ", edit);
 
       // Replace elements in HTML
-
-
       outputHTML = myHTML.replace("<map></map>", map.outerHTML);
 
       // change map size
@@ -104,7 +81,7 @@ function insertThisFile(myURL) {
       outputHTML = outputHTML.replace("<copy></copy>", copy.outerHTML);
       outputHTML = outputHTML.replace("<edit></edit>", edit.outerHTML);
 
-      console.log("outputHTML is: ", outputHTML);
+      // console.log("outputHTML is: ", outputHTML);
 
       $(".strava_clone").append(outputHTML);
     }
