5d77f1b73a07a97234b5fec0797dfd254c1dcb52
Max Soe <maxsoe@gmail.com>
2019-11-03 16:27:43 +0400

    update URL

--- experimental/bookmarklets/test/bookmarklet.js
+++ experimental/bookmarklets/test/bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function () { 
   var jsCode = document.createElement('script'); 
-  jsCode.setAttribute('src', 'http://maxsoe.github.io/experimental/test/script.js');                  
+  jsCode.setAttribute('src', 'http://maxsoe.github.io/experimental/bookmarklets/test/script.js');                  
 document.body.appendChild(jsCode); 
 }());
 
