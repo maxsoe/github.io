2d4b093b54b1ed72120c59f79c21176ce6911149
Max Soe <maxsoe@gmail.com>
2019-11-03 18:09:27 +0400

    format script.js in test

--- experimental/bookmarklets/background/bookmarklet.js
+++
@@ -1,7 +0,0 @@
-javascript: (function() {
-  document.body.appendChild(document.createElement("script")).src =
-  "http://maxsoe.github.io/experimental/bookmarklets/test/script.js"
-})();
-
-// "http://maxsoe.github.io/experimental/bookmarklets/test/script.js";
-// "http://caiorss.github.io/commandpanel.js";
--- experimental/bookmarklets/test/bookmarklet.js
+++ experimental/bookmarklets/test/bookmarklet.js
@@ -1,6 +1,6 @@
 javascript: (function() {
   document.body.appendChild(document.createElement("script")).src =
-  "http://maxsoe.github.io/experimental/bookmarklets/test/script.js"
+    "http://maxsoe.github.io/experimental/bookmarklets/test/script.js";
 })();
 
 // "http://maxsoe.github.io/experimental/bookmarklets/test/script.js";
