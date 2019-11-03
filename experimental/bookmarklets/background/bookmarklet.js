3d04c6eb27bfb3c908888bfe923db28d5b7a86ab
Max Soe <maxsoe@gmail.com>
2019-11-03 17:57:13 +0400

    set up github.io with redbackground file

---
+++ experimental/bookmarklets/background/bookmarklet.js
@@ -0,0 +1,7 @@
+javascript: (function() {
+  document.body.appendChild(document.createElement("script")).src =
+  "http://maxsoe.github.io/experimental/bookmarklets/test/script.js"
+})();
+
+// "http://maxsoe.github.io/experimental/bookmarklets/test/script.js";
+// "http://caiorss.github.io/commandpanel.js";
