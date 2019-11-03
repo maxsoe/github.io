12bf61b57350a695c3a918df5bad6878321a58b1
Max Soe <maxsoe@gmail.com>
2019-11-03 18:16:53 +0400

    bookmarklets: vidads

---
+++ experimental/bookmarklets/vidads/bookmarklet.js
@@ -0,0 +1,4 @@
+javascript: (function() {
+  document.body.appendChild(document.createElement("script")).src =
+  "http://maxsoe.github.io/experimental/bookmarklets/vidads/script.js";
+})();
---
+++ experimental/bookmarklets/vidads/script.js
@@ -0,0 +1,4 @@
+var sideAds = document.getElementById("video-ad");
+sideAds.style.display="none";
+var footerAds = document.getElementById("ad-footer");
+footerAds.style.display="none";
