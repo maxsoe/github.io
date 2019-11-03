f5c2c7ebc82e63568317280d84c4fe10d6f9747c
Max Soe <maxsoe@gmail.com>
2019-11-03 16:36:36 +0400

    playing around with test

--- experimental/bookmarklets/test/bookmarklet.js
+++ experimental/bookmarklets/test/bookmarklet.js
@@ -1,7 +1,12 @@
-javascript: (function () { 
-  var jsCode = document.createElement('script'); 
-  jsCode.setAttribute('src', 'http://maxsoe.github.io/experimental/bookmarklets/test/script.js');                  
-document.body.appendChild(jsCode); 
-}());
+javascript: (function() {
+  // var jsCode = document.createElement("script");
+  // jsCode.setAttribute(
+  //   "src",
+  //   "http://maxsoe.github.io/experimental/bookmarklets/test/script.js"
+  // );
+  // document.body.appendChild(jsCode);
+
+  document.body.style.background = "pink";
+})();
 
 // Use this as bookmarklet - http://maxsoe.github.io/experimental/bookmarklets/test/bookmarklet.js
